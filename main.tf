f928d427b75a04be306813c0223588dc7d20a60b
Rudra <Rudrayyaswamims@gmail.com>
2023-06-26 15:10:41 +0530

    updating tf-module-docdb

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ resource "aws_docdb_cluster" "main" {
   master_password         = data.aws_ssm_parameter.pass.value
   backup_retention_period = var.backup_retention_period
   preferred_backup_window = var.preferred_backup_window
-  skip_final_snapshot     = true                        ## terraform can delete the resource created otherwise wont delete
+  skip_final_snapshot     = var.skip_final_snapshot                        ## terraform can delete the resource created otherwise wont delete
   db_subnet_group_name    = aws_docdb_subnet_group.main.name
   kms_key_id              = data.aws_kms_key.key.arn
-  storage_encrypted       = true     # by default it is false
+  storage_encrypted       = var.storage_encrypted     # by default it is false
 }
 
 ## create subnet group ids
